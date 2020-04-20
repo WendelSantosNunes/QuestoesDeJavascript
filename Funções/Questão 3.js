f47f8be17f23748ddf020b9f7c3f3f6fceaa3944
Wendel dos Santos Nunes <wendelnunes999@hotmail.com>
2020-04-20 16:39:02 -0300

    Update Questão 3.js

--- Funções/Questão 3.js
+++ Funções/Questão 3.js
@@ -1,4 +1,9 @@
-/*Uma das vantagens da programação é a automatização de tarefas que não gostamos de realizar. Dito isto, elabore uma função cujo objetivo é resolver a fórmula de Bhaskara. Para isso, sua função deve receber três parâmetros, “ax2”, “bx” e “c”, de tal modo que na equação: 3x2 - 5x + 12 os valores seriam respectivamente: 3, -5, 12. Como retorno deve ser passado um vetor que tem 2 valores um para cada possível resultado, mesmo que os resultados sejam iguais. Caso o delta seja negativo, retorne, ao invés do vetor, um string com a frase: “Delta é negativo”.*/
+/* Uma das vantagens da programação é a automatização de tarefas que não gostamos de realizar. Dito isto,
+elabore uma função cujo objetivo é resolver a fórmula de Bhaskara. Para isso, sua função deve receber três
+parâmetros, “ax2”, “bx” e “c”, de tal modo que na equação: 3x2 - 5x + 12 os valores seriam respectivamente:
+3,-5, 12. Como retorno deve ser passado um vetor que tem 2 valores um para cada possível resultado, mesmo 
+que os resultados sejam iguais. Caso o delta seja negativo, retorne, ao invés do vetor, um string com a frase:
+“Delta é negativo”.*/
 
 bhaskara = (a,b,c) =>{
     let resultado = []
@@ -14,4 +19,4 @@ bhaskara = (a,b,c) =>{
 }
 
 console.log(bhaskara(1,3,2))// exemplo
-// -> [ 1, 2 ]
+// ->[ 1, 2 ]
