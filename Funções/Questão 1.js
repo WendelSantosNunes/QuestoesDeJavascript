4e6453623572ca855d69b658eed159281173e629
Wendel dos Santos Nunes <wendelnunes999@hotmail.com>
2020-04-20 16:15:11 -0300

    atualização

--- Funções/Questão 1.js
+++ Funções/Questão 1.js
@@ -4,4 +4,5 @@ multiplicação e divisão desses valores. */
 function operador (n1,n2){
     console.log(n1+n2,n1-n2,n1*n2,n1/n2)
 }
-operador(2,3)
+operador(2,3)//valores de exemplo
+//->5 -1 6 0.6666666666666666
