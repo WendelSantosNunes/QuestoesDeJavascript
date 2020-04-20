ef765203ebe701f51eb1ef03b667889b23ec8408
Wendel dos Santos Nunes <wendelnunes999@hotmail.com>
2020-04-20 16:20:33 -0300

    atualização

---
+++ Funções/Questão 2.js
@@ -0,0 +1,9 @@
+/*​ 1º) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.*/
+
+elevacao = (n1,n2) =>{
+    let result = Math.pow(n1,n2)
+    return result 
+}
+
+console.log(elevacao(3,9))// exemplo
+// -> 19683
