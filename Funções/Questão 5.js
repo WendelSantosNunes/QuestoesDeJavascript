094e4a60ebb93fd7b6ffa8788b310db215f685ef
Wendel dos Santos Nunes <63923854+WendelSantosNunes@users.noreply.github.com>
2020-04-21 15:55:48 -0300

    Update Questão 5.js

--- Funções/Questão 5.js
+++ Funções/Questão 5.js
@@ -1,8 +1,8 @@
-/*5º)​ Lidar com números em JavaScript pode dar muita dor de cabeça. Você já viu o que acontece quando faz o
-seguinte comando no console: console.log(0.1 + 0.2); O resultado será: 0.30000000000000004. Outra coisa
-importante de observar, é o fato que o ponto é utilizado no lugar da vírgula e vice versa. Com isso, vamos fazer
-um exercício simples para mostrar dinheiro sempre da forma correta. Desenvolva uma função JavaScript para
-que ela receba um valor como 0.30000000000000004 e retorne R$0,30 (observe a vírgula e o ponto).*/
+/*5º)​ Lidar com números em JavaScript pode dar muita dor de cabeça. Você já viu o que acontece quando faz o seguinte comando no
+console: console.log(0.1 + 0.2); O resultado será: 0.30000000000000004. Outra coisa importante de observar, é o fato que o ponto
+é utilizado no lugar da vírgula e vice versa. Com isso, vamos fazer um exercício simples para mostrar dinheiro sempre da forma 
+correta. Desenvolva uma função JavaScript para que ela receba um valor como 0.30000000000000004 e retorne R$0,30 (observe a 
+vírgula e o ponto).*/
 
 function dinheiro (n1,n2){
     let valor = n1+n2
@@ -10,9 +10,3 @@ function dinheiro (n1,n2){
 }
 dinheiro(0.1 , 0.2) //exemplo
 // -> R$ 0,30
-
-/* Explicação sobrea as funções:
-    toFixed  -> Determinar quantos zeros ou números irão ficar depois da virgula
-    toString -> Transformar o número em uma string
-    replace  -> troca uma string por outra ( isso resumo, pois essa função faz mais coisas.)
-*/
