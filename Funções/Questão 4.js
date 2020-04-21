d037fba367874eeb7b325002d5c370bdb1d58fa2
Wendel dos Santos Nunes <wendelnunes999@hotmail.com>
2020-04-21 15:42:15 -0300

    Update

---
+++ Funções/Questão 4.js
@@ -0,0 +1,9 @@
+/*1º)Crie um função que irá recebr dois valores, o dividendo e o divisor. A função deverá imprimir o resultado e o resto da divisão dois valores.*/
+divisao = (n1,n2) => {
+    console.log('Resultado:' + Math.floor(n1/n2)) // a redonda o número
+    console.log('Resto:' + n1%n2)
+}
+
+divisao(11,4) // exemplo
+// -> Resultado:4
+// -> Resto:3
