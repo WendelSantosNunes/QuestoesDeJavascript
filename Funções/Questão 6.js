a08f0f62f2fe96d575586ca3b8a4881beb45ef8a
Wendel dos Santos Nunes <wendelnunes999@hotmail.com>
2020-04-22 23:16:05 -0300

    Create Questão 6.js

---
+++ Funções/Questão 6.js
@@ -0,0 +1,7 @@
+/*10º) ​ Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true ou false.*/
+divisivel = (n) => {
+    console.log(n % 3 === 0)
+}
+
+divisivel(2)// exemplo
+// -> false
