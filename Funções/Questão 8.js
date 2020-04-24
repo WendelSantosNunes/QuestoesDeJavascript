7f721a985be96537bf613a5745728ccb0c69baf8
Wendel dos Santos Nunes <63923854+WendelSantosNunes@users.noreply.github.com>
2020-04-24 19:33:46 -0300

    Update Questão 8.js

--- Funções/Questão 8.js
+++ Funções/Questão 8.js
@@ -1,6 +1,6 @@
-/*06º)​Elabore duas funções que recebem três parâmetros: capital inicial, taxa de juros e tempo de aplicação. A
-primeira função retornará o montante da aplicação financeira sob o regime de juros simples e a segunda
-retornará o valor da aplicação sob o regime de juros compostos.*/
+/*08º)​Elabore duas funções que recebem três parâmetros: capital inicial, taxa de juros e tempo de aplicação. A primeira função
+retornará o montante da aplicação financeira sob o regime de juros simples e a segunda retornará o valor da aplicação sob o 
+regime de juros compostos.*/
 
 jurosSimples = (cap,taxa,tempo) =>{
     return cap + (cap * taxa * tempo)
