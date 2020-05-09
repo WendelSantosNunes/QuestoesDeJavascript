08aab866f31ec8004135e7f19633504d22dddc8c
Wendel dos Santos Nunes <63923854+WendelSantosNunes@users.noreply.github.com>
2020-05-08 22:48:44 -0300

    Update Questão19.js

--- Funções/Questão19.js
+++ Funções/Questão19.js
@@ -1,6 +1,7 @@
-/*19º)​ Utilizando a estrutura do Switch faça um programa que simule uma calculadora básicaO programa recebe
-como parâmetros dois valores numéricos e uma string referente à operação e a realize com os valoresnuméricos na ordem que foram inseridos. Por exemplo: calculadora (2, ‘+’, 3). A função efetuará a soma de 2 e
-3. Dica: Os sinais das operações são: ‘+’. ‘-’, ‘*’ e ‘/’. Crie um caso default para operações inválidas.*/
+/*19º)​ Utilizando a estrutura do Switch faça um programa que simule uma calculadora básicaO programa recebe como parâmetros 
+dois valores numéricos e uma string referente à operação e a realize com os valoresnuméricos na ordem que foram inseridos. 
+Por exemplo: calculadora (2, ‘+’, 3). A função efetuará a soma de 2 e 3. Dica: Os sinais das operações 
+são: ‘+’. ‘-’, ‘*’ e ‘/’. Crie um caso default para operações inválidas.*/
 
 function calculadora (n1,op,n2) {
     switch(op){
