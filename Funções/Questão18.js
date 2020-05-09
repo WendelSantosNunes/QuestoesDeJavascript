e8ef096f75a74eb2ca8e767bc7e814046b075d77
Wendel dos Santos Nunes <63923854+WendelSantosNunes@users.noreply.github.com>
2020-05-08 22:47:23 -0300

    Update Questão18.js

--- Funções/Questão18.js
+++ Funções/Questão18.js
@@ -1,10 +1,9 @@
-/*18º)​ Crie um programa para informar quais e quantas notas são necessárias para entregar o mínimo de cédulas
-para um determinado valor informado pelo usuário considerando notas de R$ 100, R$ 50, R$ 10 e R$ 5 e R$ 1.
-Seu programa deve mostrar apenas as notas utilizadas. Por exemplo, ao solicitar R$18, o programa deve
-informar apenas a seguinte informação (note que não foram exibidas informações sobre as demais cédulas): 1
-nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1.*/
+/*18º)​ Crie um programa para informar quais e quantas notas são necessárias para entregar o mínimo de cédulas para um 
+determinado valor informado pelo usuário considerando notas de R$ 100, R$ 50, R$ 10 e R$ 5 e R$ 1. Seu programa deve mostrar
+apenas as notas utilizadas. Por exemplo, ao solicitar R$18, o programa deve informar apenas a  seguinte informação (note que
+não foram exibidas informações sobre as demais cédulas): 1 nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1.*/
 
-// -> Eu achei essa questão muito interessante, e por causa disso, também fiz a parte dos centavos
+// -> Eu achei essa questão muito interessante, e por causa disso, também fiz a parte dos centavos.
 
 let cedulas = function (dinheiro,){
     let Notas = [100,50,10,5,1,0.50,0.25,0.10,0.05,0.01]
