4f3eb811dc966757fe5fd650e850f97e23e5d11a
Wendel dos Santos Nunes <wendelnunes999@hotmail.com>
2020-05-11 18:25:38 -0300

    Create Questão22.js

---
+++ Funções/Questão22.js
@@ -0,0 +1,11 @@
+/*22º)​ Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.*/
+
+repetição = ( ) => { 
+    let i = 0
+    while( i < 11){
+        console.log('Hello World')
+        i++
+    }
+}
+
+repetição();
