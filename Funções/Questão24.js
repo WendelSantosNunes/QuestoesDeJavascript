a87d7929148b7b5a8f15664c83921228a3f9875d
Wendel dos Santos Nunes <63923854+WendelSantosNunes@users.noreply.github.com>
2020-05-14 20:28:12 -0300

    Update Questão24.js

--- Funções/Questão24.js
+++ Funções/Questão24.js
@@ -1,7 +1,7 @@
-/*24º)​ Criar uma função para calcular o valor a ser pago de anuidade de uma associação. A função recebe como
-parâmetro um inteiro que representa o mês (1 - janeiro, 2 - fevereiro...) que foi paga e o valor da anuidade. A
-anuidade deve ser paga no mês de janeiro. Por mês, é cobrado 5% de juros (sob o regime de juros
-compostos). O retorno deve ser o valor a ser pago para o respectivo mês escolhido.*/
+/*24º)​ Criar uma função para calcular o valor a ser pago de anuidade de uma associação. A função recebe como parâmetro um 
+inteiro que representa o mês (1 - janeiro, 2 - fevereiro...) que foi paga e o valor da anuidade. A anuidade deve ser paga no
+mês de janeiro. Por mês, é cobrado 5% de juros (sob o regime de juros compostos). O retorno deve ser o valor a ser pago para o 
+respectivo mês escolhido.*/
 
 anuidade = (mes,valor) => {
     if(mes >= 1 && mes <= 12){
