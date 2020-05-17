e8870e644ee51567cdd40f4ba2c2ce4dafedfed0
Wendel dos Santos Nunes <63923854+WendelSantosNunes@users.noreply.github.com>
2020-05-17 19:16:03 -0300

    Update Questão27.js

--- Funções/Questão27.js
+++ Funções/Questão27.js
@@ -1,6 +1,6 @@
-/* 27º)​ Construa uma função que receba como parâmetros as alturas e as taxas de crescimento anuais de duas
-crianças e calcule se existe uma criança menor, caso exista se a criança menor ultrapassará a maior e em
-quantos anos isso acontecerá. Utilize centímetros para as unidades de medida.*/
+/* 27º)​ Construa uma função que receba como parâmetros as alturas e as taxas de crescimento anuais de duas crianças e calcule
+se existe uma criança menor, caso exista se a criança menor ultrapassará a maior e em quantos anos isso acontecerá. Utilize 
+centímetros para as unidades de medida.*/
 
 crescer = (altura1,taxa1,altura2,taxa2) => {
     if(altura1 == altura2){
